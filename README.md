1d16ec894d87a683aa0fa6542496f0e53e6ae0c6
Mike Rogers <me@mikerogers.io>
2019-05-04 21:45:45 +0100

    Hanlding shutdowns more gracefully

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Lastly in your `config/puma.rb` file, append the line:
 
 
 ```ruby
-plugin('ngrok-tunnel') if ENV['NGROK_TUNNEL_ENABLED']
+plugin :ngrok_tunnel if ENV['NGROK_TUNNEL_ENABLED']
 ```
 
 ## Installation with puma-dev
@@ -40,10 +40,10 @@ CONFIG=config/puma.rb
 
 Read about how to configure puma to use this plugin here: https://github.com/puma/puma#plugins
 
-There are two variables this plugin reads from the environment which control its behavior.
+There are a few variables this plugin reads from the environment which control its behavior.
 
-* `WEB_CONCURRENCY` — How many workers to run. This will be ignored on JRuby and Windows, where only 1 worker will be run.
-* `MAX_THREADS` — The number of threads to run per worker. Note that this also sets the minimum number of threads to the same value, which is a recommended approach, especially in a single-app environment such as Heroku.
+* `TODO`
+
 
 ## License
 
--- lib/puma/plugin/ngrok_tunnel.rb
+++ lib/puma/plugin/ngrok_tunnel.rb
@@ -1,16 +1,26 @@
+require 'ngrok/tunnel'
+
 Puma::Plugin.create do
-  def start(_launcher)
-    require 'ngrok/tunnel'
-    options = {
-      addr: ENV.fetch('PORT') { 3000 },
+  def start(_dsl)
+    in_background do
+      puts '[puma-ngrok-tunnel] Starting'
+      puts '[puma-ngrok-tunnel] Tunneling at: ' + Ngrok::Tunnel.start(options)
+      at_exit do
+        puts '[puma-ngrok-tunnel] Stopping'
+        Ngrok::Tunnel.stop
+      end
+    end
+  end
+
+  def options
+    @options = {
+      addr: ENV.fetch('NGROK_ADDR') { nil } || ENV.fetch('PORT') { 3000 },
       authtoken: ENV.fetch('NGROK_AUTHTOKEN') { nil },
       host_header: ENV.fetch('NGROK_HOST_HEADER') { nil },
       config: File.join(ENV['HOME'], '.ngrok2', 'ngrok.yml'),
       subdomain: ENV.fetch('NGROK_SUBDOMAIN') { nil },
       region: ENV.fetch('NGROK_REGION') { nil },
       hostname: ENV.fetch('NGROK_HOSTNAME') { nil }
-    }.reject { |_, value| value.blank? }
-
-    puts '[NGROK] tunneling at ' + Ngrok::Tunnel.start(options)
+    }.reject { |_, value| value.nil? }
   end
 end
